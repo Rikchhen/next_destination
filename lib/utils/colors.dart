874f6560bad3c5f5d89f44303d9d8afe6e71c135
Rikchhen <rikchhenlama3@gmail.com>
2025-12-05 17:51:10 +0545

    colors.dart added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primaryRed = Color(0xFFD32F2F);
+const Color secondaryText = Colors.grey;
+
+const Color appBackground = Color(0xFFE0E0E0);
+
+const Color gradientStartColor = Color(0xFFFEEEF0);
+const Color gradientEndColor = Color(0xFFF8F8F8);

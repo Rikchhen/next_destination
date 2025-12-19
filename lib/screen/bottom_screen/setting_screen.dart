83f7850adec7b344e3dfdf9c4e34731b5c367143
Rikchhen <rikchhenlama3@gmail.com>
2025-12-19 15:30:15 +0545

    bottom screen added

---
+++ lib/screen/bottom_screen/setting_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Center(child: Text("welcome to Setting Screen")),
+    );
+  }
+}
---
+++ lib/screen/bottom_screen/wallet_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WalletScreen extends StatefulWidget {
+  const WalletScreen({super.key});
+
+  @override
+  State<WalletScreen> createState() => _WalletScreenState();
+}
+
+class _WalletScreenState extends State<WalletScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Center(child: Text("welcome to Wallet Screen")),
+    );
+  }
+}

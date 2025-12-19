1a3d20e6477040cc5b3b629d716ffeb8e1e9d9b1
Rikchhen <rikchhenlama3@gmail.com>
2025-12-19 15:19:05 +0545

    App.dart modified

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:next_destination/theme/theme_data.dart';
 import 'package:next_destination/utils/colors.dart';
+
 import 'package:next_destination/screen/splash_screen.dart';
 
 class App extends StatelessWidget {
@@ -10,29 +12,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Next Destination',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: primaryRed,
-        primarySwatch: Colors.red,
-        fontFamily: 'Roboto',
-
-        inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.grey.shade100,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(15.0),
-            borderSide: BorderSide.none,
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(15.0),
-            borderSide: BorderSide.none,
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(15.0),
-            borderSide: BorderSide(color: primaryRed, width: 2),
-          ),
-        ),
-      ),
-
+      theme: getApplicationTheme(),
       home: const SplashScreen(),
     );
   }

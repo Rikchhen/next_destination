211a606dfbe88170a3bfa93dce766189e250624c
Rikchhen <rikchhenlama3@gmail.com>
2026-01-02 16:43:51 +0545

    auth functional

--- lib/utils/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryRed = Color(0xFFD32F2F);
-const Color secondaryText = Colors.grey;
-
-const Color appBackground = Color(0xFFE0E0E0);
-
-const Color gradientStartColor = Color(0xFFFEEEF0);
-const Color gradientEndColor = Color(0xFFF8F8F8);

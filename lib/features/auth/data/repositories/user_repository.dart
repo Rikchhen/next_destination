7570a04d454660eb8bfb2e82b03cb43ff6bfcff9
Rikchhen <rikchhenlama3@gmail.com>
2026-01-16 16:36:51 +0545

    lib/features/auth/data/repositories/user_repository.dart modified

--- lib/features/auth/data/repositories/user_repository.dart
+++ lib/features/auth/data/repositories/user_repository.dart
@@ -1,32 +1,49 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:next_destination/core/error/failures.dart';
+import 'package:next_destination/core/services/connectivity/network_info.dart';
 import 'package:next_destination/features/auth/data/datasources/local/user_local_datasource.dart';
+import 'package:next_destination/features/auth/data/datasources/remote/user_remote_datasource.dart';
 import 'package:next_destination/features/auth/data/datasources/user_datasource.dart';
+import 'package:next_destination/features/auth/data/models/user_api_model.dart';
 import 'package:next_destination/features/auth/data/models/user_hive_model.dart';
 import 'package:next_destination/features/auth/domain/entities/user_entity.dart';
 import 'package:next_destination/features/auth/domain/repositories/user_repositroy.dart';
 
 final userRepositoryProvider = Provider<IUserRepository>((ref) {
-  final userDatasource = ref.read(userLocalDatasourceProvider);
-  return UserRepository(userdataSource: userDatasource);
+  final userLocalDatasource = ref.read(userLocalDatasourceProvider);
+  final userRemoteDatasource = ref.read(userRemoteDatasourceProvider);
+  final networkInfo = ref.read(networkInfoProvider);
+  return UserRepository(
+    userLocalDataSource: userLocalDatasource,
+    userRemoteDatasource: userRemoteDatasource,
+    networkInfo: networkInfo,
+  );
 });
 
 class UserRepository implements IUserRepository {
-  final IUserDatasource _userDatasource;
+  final IUserLocalDatasource _userLocalDatasource;
+  final IUserRemoteDatasource _userRemoteDatasource;
+  final NetworkInfo _networkInfo;
 
-  UserRepository({required IUserDatasource userdataSource})
-    : _userDatasource = userdataSource;
+  UserRepository({
+    required IUserLocalDatasource userLocalDataSource,
+    required IUserRemoteDatasource userRemoteDatasource,
+    required NetworkInfo networkInfo,
+  }) : _userLocalDatasource = userLocalDataSource,
+       _userRemoteDatasource = userRemoteDatasource,
+       _networkInfo = networkInfo;
 
   @override
   Future<Either<Failure, UserEntity>> getCurrentUser() async {
     try {
-      final user = await _userDatasource.getCurrentUser();
+      final user = await _userLocalDatasource.getCurrentUser();
       if (user != null) {
         final userEntity = user.toEntity();
         return Right(userEntity);
       }
-      return Left(LocalDatabaseFailure(message: "Couldnot get current user"));
+      return Left(LocalDatabaseFailure(message: "Could not get current user"));
     } catch (e) {
       return Left(LocalDatabaseFailure(message: e.toString()));
     }
@@ -37,26 +54,60 @@ class UserRepository implements IUserRepository {
     String phoneNumber,
     String password,
   ) async {
-    try {
-      final user = await _userDatasource.loginUser(phoneNumber, password);
-      if (user != null) {
-        final userEntity = user.toEntity();
-        return Right(userEntity);
+    if (await _networkInfo.isConnected) {
+      try {
+        final apiModel = await _userRemoteDatasource.loginUser(
+          phoneNumber,
+          password,
+        );
+        if (apiModel != null) {
+          final entity = apiModel.toEntity();
+          return Right(entity);
+        }
+        return const Left(ApiFailure(message: "Invalid Credentials"));
+      } on DioException catch (e) {
+        return Left(
+          ApiFailure(
+            message: e.response?.data['message'] ?? "login Failed",
+            statusCode: e.response?.statusCode,
+          ),
+        );
+      } catch (e) {
+        return Left(ApiFailure(message: e.toString()));
       }
-      return Left(LocalDatabaseFailure(message: "Failed to Log In User"));
-    } catch (e) {
-      return Left(LocalDatabaseFailure(message: e.toString()));
+    } else {
+      try {
+        final user = await _userLocalDatasource.loginUser(
+          phoneNumber,
+          password,
+        );
+        if (user != null) {
+          final userEntity = user.toEntity();
+          return Right(userEntity);
+        }
+        return Left(LocalDatabaseFailure(message: "Failed to Log In User"));
+      } catch (e) {
+        return Left(LocalDatabaseFailure(message: e.toString()));
+      }
     }
   }
 
   @override
   Future<Either<Failure, bool>> logout() async {
     try {
-      final result = await _userDatasource.logout();
+      final result = await _userLocalDatasource.logout();
       if (result) {
         return Right(true);
       }
       return Left(LocalDatabaseFailure(message: "Cannot Log User Out"));
+    } on DioException catch (e) {
+      //We use DioException to catch all API errors [status codes and shit]
+      return Left(
+        ApiFailure(
+          message: e.response?.data['message'] ?? "Logout Failed",
+          statusCode: e.response?.statusCode,
+        ),
+      );
     } catch (e) {
       return Left(LocalDatabaseFailure(message: e.toString()));
     }
@@ -64,16 +115,30 @@ class UserRepository implements IUserRepository {
 
   @override
   Future<Either<Failure, bool>> registerUser(UserEntity entity) async {
-    try {
-      // Here We convert the incoming entity into model.
-      final model = UserHiveModel.fromEntity(entity);
-      final result = await _userDatasource.registerUser(model);
-      if (result) {
-        return Right(true);
+    if (await _networkInfo.isConnected) {
+      try {
+        final apimodel = UserApiModel.fromEntity(entity);
+        await _userRemoteDatasource.registerUser(apimodel);
+        return const Right(true);
+      } on DioException catch (e) {
+        return Left(
+          ApiFailure(
+            message: e.response?.data['message'] ?? "Registration Failed",
+            statusCode: e.response?.statusCode,
+          ),
+        );
+      } catch (e) {
+        return Left(LocalDatabaseFailure(message: e.toString()));
       }
-      return Left(LocalDatabaseFailure(message: "Failed to Register User"));
-    } catch (e) {
-      return Left(LocalDatabaseFailure(message: e.toString()));
+    } else {
+      try {
+        // Here We convert the incoming entity into model.
+        final model = UserHiveModel.fromEntity(entity);
+        await _userLocalDatasource.registerUser(model);
+        return Right(true);
+      } catch (e) {
+        return Left(LocalDatabaseFailure(message: e.toString()));
+      }
     }
   }
 }

a5374b6f6ac2ab03e2b297c74c84f438a3eed0b9
Rikchhen <rikchhenlama3@gmail.com>
2026-03-08 22:40:41 +0545

    lib/app/app.dart modified

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:next_destination/app/theme/theme_data.dart';
+import 'package:next_destination/app/theme/theme_mode_controller.dart';
 import 'package:next_destination/features/splash/presentation/pages/splash_screen.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
   @override
+  ConsumerState<App> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> {
+  @override
   Widget build(BuildContext context) {
+    final themeMode = ref.watch(activeThemeModeProvider);
+
     return MaterialApp(
       title: 'Next Destination',
       debugShowCheckedModeBanner: false,
       theme: getApplicationTheme(),
+      darkTheme: getDarkApplicationTheme(),
+      themeMode: themeMode,
       home: const SplashScreen(),
     );
   }
